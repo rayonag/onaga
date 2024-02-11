fef0a9e7f5bac24fb6f49cbae45a9740c63409d7
Ray <ronaga@bridgesforpeace.com>
2024-02-11 10:26:50 +0200

    com

--- src/app/kakeibo/components/Numkeys.tsx
+++ src/app/kakeibo/components/Numkeys.tsx
@@ -16,17 +16,27 @@ const Numkeys: FC<NumkeysProps> = ({ amount, setAmount }) => {
         }
     };
     const [touched, setTouched] = useState(0);
-    const buttonStyle = (num: number) => `${touched == num ? "bg-gray-300 " : "bg-stone-400 "}w-16 h-16 text-white  rounded-full flex items-center justify-center text-4xl shadow-md transition-colors`;
+    const buttonStyle = (num: number) => `${touched == num ? "bg-gray-300 " : "bg-stone-400 "}w-16 h-16 text-white rounded-full items-center justify-center text-4xl shadow-md transition-colors`;
     const divStyle = "flex justify-between m-2";
     return (
         <div className="justify-center content-center w-3/5">
             <div className={divStyle}>
                 {[1, 2, 3].map((number) => (
-                    <input className={buttonStyle(number)} type="button" value={number} onTouchStart={() => setTouched(number)} onClick={() => handleOnClick(number.toString())} />
+                    <div>
+                        <button className={buttonStyle(number)} type="button" value={number} onTouchStart={() => setTouched(number)} onTouchEnd={() => setTouched(0)} onClick={() => handleOnClick(number.toString())}>
+                            <div>{number}</div>
+                            <div className="text-sm">{number == 1 ? "" : 2 ? "ABC" : 3 ? "DEF" : ""}</div>
+                        </button>
+                    </div>
                 ))}
             </div>
             <div className={divStyle}>
                 {[4, 5, 6].map((number) => (
+                    <input className={buttonStyle(number)} type="button" value={number} onClick={() => handleOnClick(number.toString())} />
+                ))}
+            </div>
+            <div className={divStyle}>
+                {[7, 8, 9].map((number) => (
                     <input className={buttonStyle(number)} type="button" value={number} onClick={() => handleOnClick(number.toString())} />
                 ))}
             </div>
