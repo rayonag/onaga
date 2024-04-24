05565c55f237379de9b669c69ccec18b5e4f8e21
Ray <ronaga@bridgesforpeace.com>
2024-04-24 07:09:21 +0300

    pwa fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="apple-touch-icon" href="/icon.png"></link>
+            <meta name="theme-color" content="#01688D" />
             <body>{children}</body>
         </html>
     );
