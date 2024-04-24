90dcaf2cdb08c146720f588b97d45d635bd2b86a
Ray <ronaga@bridgesforpeace.com>
2024-04-24 13:40:16 +0300

    commit

--- src/app/game/vocabs/views/home/home.tsx
+++ src/app/game/vocabs/views/home/home.tsx
@@ -1,7 +1,14 @@
+import { Haptics, ImpactStyle } from "@capacitor/haptics";
+
 const Home = () => {
+    const hapticsImpactMedium = async () => {
+        await Haptics.impact({ style: ImpactStyle.Medium });
+    };
     return (
         <div className="flex flex-col h-screen justify-center text-center items-center">
-            <div className="btn-theme">Coming Soon...</div>
+            <div onClick={async () => await hapticsImpactMedium} className="btn-theme">
+                Coming Soon...
+            </div>
         </div>
     );
 };
