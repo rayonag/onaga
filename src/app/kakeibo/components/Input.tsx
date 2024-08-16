2b7c4322d9ca8b9627f53a0b5e1b410f31ddecec
Ray <ronaga@bridgesforpeace.com>
2024-08-16 06:06:14 +0300

    commit

--- src/app/kakeibo/components/Input.tsx
+++ src/app/kakeibo/components/Input.tsx
@@ -12,7 +12,7 @@ const Input: FC<InputProps> = ({ value, setValue, placeholder }) => {
     return (
         <label className="my-2 w-80 me-5 md:max-w-sm">
             <div className={`font-semibold text-2xl mb-1 transition duration-500 ${isFocused || value.length || "text-gray-400 translate-y-10"}`}>{placeholder}</div>
-            <textarea rows={3} value={value} onChange={(e) => setValue(e.currentTarget.value)} className={"text-white border-b-2 py-2 px-3 bg-transparent focus:outline focus:outline-sky-500 focus:ring-4 focus:ring-sky-500/30"} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} />
+            <textarea rows={3} value={value} onChange={(e) => setValue(e.currentTarget.value)} className={"text-black border-b-2 py-2 px-3 bg-transparent focus:outline focus:outline-sky-500 focus:ring-4 focus:ring-sky-500/30"} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} />
         </label>
     );
 };
